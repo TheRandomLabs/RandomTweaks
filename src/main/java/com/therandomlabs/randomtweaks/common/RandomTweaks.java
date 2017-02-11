dcb9dc0018358aea8c639ec46a63a653ed53d644
TheRandomLabs <therandomlabsinc@gmail.com>
2017-02-11 22:39:59 +1100

    German translations updated

--- src/main/java/com/therandomlabs/randomtweaks/common/RandomTweaks.java
+++ src/main/java/com/therandomlabs/randomtweaks/common/RandomTweaks.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 		acceptableRemoteVersions = "*")
 public final class RandomTweaks {
 	public static final String MODID = "randomtweaks";
-	public static final String VERSION = "1.11.2-1.1.0.0";
+	public static final String VERSION = "1.11.2-1.1.1.0";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.10,1.12)";
 
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
