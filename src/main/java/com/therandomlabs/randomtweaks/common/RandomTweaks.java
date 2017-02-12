20396ced31722f95492b20262ced254338738ddf
TheRandomLabs <therandomlabsinc@gmail.com>
2017-02-12 12:37:24 +1100

    Release 1.11.2-1.1.1.1

--- src/main/java/com/therandomlabs/randomtweaks/common/RandomTweaks.java
+++ src/main/java/com/therandomlabs/randomtweaks/common/RandomTweaks.java
@@ -18,10 +18,11 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = RandomTweaks.MODID, version = RandomTweaks.VERSION,
 		acceptedMinecraftVersions = RandomTweaks.ACCEPTED_MINECRAFT_VERSIONS,
-		acceptableRemoteVersions = "*")
+		acceptableRemoteVersions = "*", updateJSON =
+		"https://raw.githubusercontent.com/TheRandomLabs/RandomTweaks/master/versions.json")
 public final class RandomTweaks {
 	public static final String MODID = "randomtweaks";
-	public static final String VERSION = "1.11.2-1.1.1.0";
+	public static final String VERSION = "1.11.2-1.1.1.1";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.10,1.12)";
 
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
