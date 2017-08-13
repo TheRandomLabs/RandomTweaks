8419f7e20964369500c0bb8173170b5407918c8f
TheRandomLabs <therandomlabsinc@gmail.com>
2017-08-13 18:24:44 +1000

    Remove LogFilterException

--- src/main/java/com/therandomlabs/randomtweaks/common/LogFilterException.java
+++
@@ -1,8 +0,0 @@
-package com.therandomlabs.randomtweaks.common;
-
-@SuppressWarnings("serial")
-public class LogFilterException extends Exception {
-	public LogFilterException(String message, Object... args) {
-		super(String.format(message, args));
-	}
-}
