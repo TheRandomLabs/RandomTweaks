b59026e9cc8100f22d384e19f5731c7450723d52
TheRandomLabs <therandomlabsinc@gmail.com>
2017-09-09 15:40:18 +1000

    Fix crash on 1.10.2

--- src/main/java/com/therandomlabs/randomtweaks/client/CapeHandler.java
+++ src/main/java/com/therandomlabs/randomtweaks/client/CapeHandler.java
@@ -12,6 +12,7 @@ import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
 import com.therandomlabs.randomtweaks.common.RTConfig;
 import com.therandomlabs.randomtweaks.common.RandomTweaks;
+import com.therandomlabs.randomtweaks.util.Compat;
 import com.therandomlabs.randomtweaks.util.Utils;
 import com.therandomlabs.randomtweaks.util.Wrapper;
 import net.minecraft.client.Minecraft;
@@ -27,7 +28,7 @@ import net.minecraftforge.fml.relauncher.Side;
 
 @EventBusSubscriber(value = Side.CLIENT, modid = RandomTweaks.MODID)
 public final class CapeHandler {
-	public static final Method GET_PLAYER_INFO = ReflectionHelper.findMethod(
+	public static final Method GET_PLAYER_INFO = Compat.findMethod(
 			AbstractClientPlayer.class, "getPlayerInfo", "func_175155_b");
 	public static final Field PLAYER_TEXTURES = ReflectionHelper.findField(NetworkPlayerInfo.class,
 					"playerTextures", "field_187107_a");
