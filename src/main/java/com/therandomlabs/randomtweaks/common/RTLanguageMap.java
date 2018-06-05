15fed7283d2d9e6633e287c460f6afefb7c547dd
TheRandomLabs <therandomlabsinc@gmail.com>
2018-06-05 22:48:17 +1000

    Update RTLanguageMap.java

--- src/main/java/com/therandomlabs/randomtweaks/common/RTLanguageMap.java
+++ src/main/java/com/therandomlabs/randomtweaks/common/RTLanguageMap.java
@@ -1,23 +1,12 @@
 package com.therandomlabs.randomtweaks.common;
 
-import java.lang.reflect.Field;
-import java.util.IllegalFormatException;
-import java.util.Map;
 import com.therandomlabs.randomtweaks.util.RomanNumerals;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.util.text.translation.LanguageMap;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class RTLanguageMap extends LanguageMap {
-	public static final Field LANGUAGE_LIST =
-			ReflectionHelper.findField(LanguageMap.class, "languageList", "field_74816_c");
-
-	public final Map<String, String> languageList;
-
-	@SuppressWarnings("unchecked")
 	public RTLanguageMap(LanguageMap languageMap) throws Exception {
-		languageList = (Map<String, String>) LANGUAGE_LIST.get(languageMap);
-		LANGUAGE_LIST.set(this, languageList);
+		this.languageList = languageMap.languageList;
 	}
 
 	@Override
@@ -44,23 +33,9 @@ public class RTLanguageMap extends LanguageMap {
 		return isLevel ? RomanNumerals.get(level) : key;
 	}
 
-	@Override
-	public synchronized String translateKeyFormat(String key, Object... args) {
-		try {
-			return String.format(translateKey(key), args);
-		} catch(IllegalFormatException ex) {
-			return "Format error: " + key;
-		}
-	}
-
-	@Override
-	public synchronized boolean isKeyTranslated(String key) {
-		return languageList.containsKey(key);
-	}
-
 	@SuppressWarnings("deprecation")
 	public static void replaceLanguageMaps() throws Exception {
-		RTLanguageMap instance = new RTLanguageMap(I18n.localizedName);
+		final RTLanguageMap instance = new RTLanguageMap(I18n.localizedName);
 		I18n.localizedName = instance;
 		LanguageMap.instance = instance;
 	}
