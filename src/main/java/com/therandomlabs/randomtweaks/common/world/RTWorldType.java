86633e47bde5f44b429ad2982e0f6dddb3a8ddbc
TheRandomLabs <therandomlabsinc@gmail.com>
2019-03-10 11:35:59 +1100

    Cleanup

--- src/main/java/com/therandomlabs/randomtweaks/common/world/RTWorldType.java
+++ src/main/java/com/therandomlabs/randomtweaks/common/world/RTWorldType.java
@@ -38,6 +38,7 @@ public abstract class RTWorldType extends WorldType {
 			}
 
 			final int oldLength = WORLD_TYPES.length;
+			//TODO could cause multithreading issues in 1.13
 			WORLD_TYPES = Arrays.copyOf(WORLD_TYPES, oldLength + 16);
 			WORLD_TYPES[oldLength] = this;
 			id = oldLength;
